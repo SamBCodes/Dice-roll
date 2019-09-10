7afe871c11fd07b0ca44d02ca46e754ca52266b6
SamBCodes <55123385+SamBCodes@users.noreply.github.com>
2019-09-10 09:44:40 +0530

    Add files via upload

---
+++ roll.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+while 1:
+    high = int(input("Enter Highest face:"))
+    print("Your number is", end=' ')
+    for i in range(3):
+        time.sleep(0.4)
+        print('.', end=' ')
+    time.sleep(0.4)
+    print(random.randrange(1, high+1, 1))
+    true = str(input("Roll Again(y/n)?"))
+    if true == 'n':
+        break
